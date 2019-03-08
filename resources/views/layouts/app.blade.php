013d186c2bc5f2be30d69817f3ea79531ad92574
el3zahaby <el3zahaby@gmail.com>
2019-03-08 20:05:08 +0300

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,7 +123,7 @@
             <p class="col-lg-12 footer-text text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 Copyright &copy;{{ date('y') }} All rights reserved | This all Web-site is made with
                 <i class="fa fa-heart" aria-hidden="true"></i> by
-                <a  target="_blank" href="#https://fingerprint.com"></a> &and; <a  target="_blank" href="https://elzahaby.cascocode.com">A.El-zahaby</a>,
+                <a  target="_blank" href="#https://fingerprint.com">Finger Print </a> & <a  target="_blank" href="https://elzahaby.cascocode.com">A.El-zahaby</a>,
                 <a style="font-size: 2px" href="https://colorlib.com" target="_blank">Colorlib</a>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
         </div>
