e4bd6a7366cce31db389fb78df1e986f7ec5655c
el3zahaby <el3zahaby@gmail.com>
2019-03-09 15:49:07 +0300

    undo last commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,8 @@
             <p class="col-lg-12 footer-text text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 Copyright &copy;{{ date('y') }} All rights reserved | This all Web-site is made with
                 <i class="fa fa-heart" aria-hidden="true"></i> by
-                <a  target="_blank" href="#https://fingerprint.com">Finger Print </a> & <a  target="_blank" href="https://elzahaby.cascocode.com">A.El-zahaby</a>,
+                <a  target="_blank" href="#https://fingerprint.com">Finger Print </a> &
+                <a  target="_blank" href="https://elzahaby.cascocode.com">A.El-zahaby</a>
                 <a style="font-size: 2px" href="https://colorlib.com" target="_blank">Colorlib</a>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
         </div>
@@ -154,7 +155,6 @@
 
 @stack("js")
 <script id="dsq-count-scr" src="//rrd-2.disqus.com/count.js" async></script>
-
 </body>
 
 </html>
