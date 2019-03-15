06bab4e23906299b614bf3b28e74123141f9e450
el3zahaby <el3zahaby@gmail.com>
2019-03-15 19:04:46 +0300

    update footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,7 +113,7 @@
                 <i class="fa fa-heart" aria-hidden="true"></i> by
                 <a  target="_blank" href="#https://fingerprint.com">Finger Print </a> &
                 <a  target="_blank" href="https://elzahaby.cascocode.com">A.El-zahaby</a>
-                <a style="font-size: 2px" href="https://colorlib.com" target="_blank">Colorlib</a>
+                <a style="font-size: 0.5px" href="https://colorlib.com" target="_blank">Colorlib</a>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
         </div>
     </div>
