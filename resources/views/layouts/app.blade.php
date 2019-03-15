ffe532412d108621087cd0b0c7a8d5cc24273351
el3zahaby <el3zahaby@gmail.com>
2019-03-15 18:59:40 +0300

    edit year format in footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
         </div>
         <div class="row footer-bottom d-flex justify-content-between align-items-center">
             <p class="col-lg-12 footer-text text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;{{ date('y') }} All rights reserved | This all Web-site is made with
+                Copyright &copy;{{ date('Y') }} All rights reserved | This all Web-site is made with
                 <i class="fa fa-heart" aria-hidden="true"></i> by
                 <a  target="_blank" href="#https://fingerprint.com">Finger Print </a> &
                 <a  target="_blank" href="https://elzahaby.cascocode.com">A.El-zahaby</a>
