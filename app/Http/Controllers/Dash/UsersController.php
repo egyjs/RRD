c3f0d08c8bf3da1956e68176bd1bef3e8b3f9dcb
Abdulrahman elzahaby <el3zahaby@gmail.com>
2019-03-28 15:56:20 +0300

    Update UsersController.php

--- app/Http/Controllers/Dash/UsersController.php
+++ app/Http/Controllers/Dash/UsersController.php
@@ -25,9 +25,7 @@ class UsersController extends Controller
 
     public function allView(){
         auth()->user()->authorizeRoles(['Users manager','manager']);  //->hasRole('Users manager');
-        $users = User::whereNotIn('id',[\auth()->user()->id])->whereHas('roles',function ($q){
-            $q->whereNotIn('name',['manager']);
-        })->get();
+        $users = User::whereNotIn('id',[\auth()->user()->id])->skip(1)->take(PHP_INT_MAX)->get();
         return view('dash.users',compact('users'));
     }
 
